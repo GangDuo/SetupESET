8f4456cda47629f68bf2e6ad5e8641cbc451cb97
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2018-05-14 15:07:39 +0900

    OSが32ビット、64ビットを判定し、ESETセキュリティ ソフトウェア インストーラーを実行する。

---
+++ SetupESET/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupESET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bin = null;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                bin = Properties.Resources.ees_nt64_JPN;
+            }
+            else
+            {
+                bin = Properties.Resources.ees_nt32_JPN;
+            }
+
+            var msi = Path.Combine(Path.GetTempPath(), "ees_nt_JPN.msi");
+            File.WriteAllBytes(msi, bin);
+            Process.Start(msi);
+        }
+    }
+}
